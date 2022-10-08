65393604e1cb50fb62268ec852d83433a8eb5e0d
rossrobino <rbrobino@gmail.com>
2022-10-08 09:05:54 -0700

    collapse/expand events table

--- src/lib/components/PastEventsTable.svelte
+++ src/lib/components/PastEventsTable.svelte
@@ -6,6 +6,9 @@
 	import H2 from "$lib/components/headings/H2.svelte";
 
 	let loading = false;
+	let showAll = false;
+	let showAllId = "showAll" + (Math.random()).toString();
+
 	export let profileId = "";
 	export let organized = false;
 	let columnNames = [];
@@ -53,8 +56,10 @@
 					.or(orString, orParams)
 					.order("created_at", { ascending: false });
 				if (error) throw new Error(error.message);
+				let uid = 0;
 				data.forEach((element) => {
 					let inList = false;
+
 					events.forEach((currentItem) => {
 						if (element.event_id === currentItem.id) {
 							inList = true;
@@ -70,6 +75,7 @@
 								first_name: element.events.profiles.first_name,
 								last_name: element.events.profiles.last_name,
 							},
+							uid: uid++,
 						});
 					}
 				});
@@ -85,6 +91,10 @@
 	function rowClick(url) {
 		goto(url);
 	}
+
+	function toggleShowAll() {
+		showAll = !showAll;
+	}
 </script>
 
 {#if loading}
@@ -97,46 +107,87 @@
 		<Table {columnNames} class="mb-8">
 			{#if events.length > 0}
 				{#each events as event}
-					<tr
-						class="cursor-pointer group"
-						on:click={() => rowClick(`/app/event/id/${event.id}`)}
-					>
-						<th
-							class="group-hover:bg-base-300 transform transition duration-250"
+					{#if event.uid < 3 || showAll}
+						<tr
+							class="cursor-pointer group"
+							on:click={() =>
+								rowClick(`/app/event/id/${event.id}`)}
 						>
-							<div>{event.name}</div>
-							<!-- <div class="text-sm opacity-50">Location</div> -->
-						</th>
+							<th
+								class="group-hover:bg-base-300 transform transition duration-250"
+							>
+								<div>{event.name}</div>
+								<!-- <div class="text-sm opacity-50">Location</div> -->
+							</th>
 
-						{#if !organized}
+							{#if !organized}
+								<td
+									class="group-hover:bg-base-300 transform transition duration-250"
+								>
+									<span>
+										{event.organizer.first_name}
+										{event.organizer.last_name}
+									</span>
+									<br />
+									<a
+										href="/app/profile/{event.organizer
+											.username}"
+										class="badge badge-secondary badge-sm"
+									>
+										@{event.organizer.username}
+									</a>
+								</td>
+							{/if}
+
 							<td
 								class="group-hover:bg-base-300 transform transition duration-250"
 							>
-								<span>
-									{event.organizer.first_name}
-									{event.organizer.last_name}
-								</span>
-								<br />
-								<a
-									href="/app/profile/{event.organizer
-										.username}"
-									class="badge badge-secondary badge-sm"
-								>
-									@{event.organizer.username}
-								</a>
+								{event.date}
 							</td>
-						{/if}
-
-						<td
-							class="group-hover:bg-base-300 transform transition duration-250"
-						>
-							{event.date}
-						</td>
-					</tr>
+						</tr>
+					{/if}
 				{/each}
+				{#if events.length > 3}
+					<tr>
+						<td class="bg-base-200 py-1 pl-2 sm:pl-4">
+							<div class="flex align-items-center">
+								<input
+									type="checkbox"
+									id={showAllId}
+									class="toggle my-2"
+									bind:checked={showAll}
+								/>
+								<label
+									for={showAllId}
+									class="label cursor-pointer pl-3"
+								>
+									<span class="label-text">
+										{#if showAll}
+											All
+										{:else}
+											Recent
+										{/if}
+									</span>
+								</label>
+							</div>
+						</td>
+						<td class="bg-base-200 p-2" />
+						{#if !organized}
+							<td class="bg-base-200 p-2" />
+						{/if}
+					</tr>
+				{/if}
 			{:else}
 				<tr>
-					<td>No events yet - <a href="/app/event/track" class="underline">start an event</a>.</td>
+					<td>
+						No events yet - <a
+							href="/app/event/track"
+							class="underline"
+						>
+							start an event
+						</a>
+						.
+					</td>
 				</tr>
 			{/if}
 		</Table>
